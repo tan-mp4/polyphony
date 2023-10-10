e9089450e2b32991f4855aa389699df70314618b
tan-mp4 <tan.bouzidi@proton.me>
2023-10-10 18:54:34 +0200

    start of the setup of the interface, containers etc... + placeholding images

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,17 +1,19 @@
 extends Control
 
 var notes:int = 0
-var notes_per_click:float = 1.0
+var notes_per_click:int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	notes = 0
+	notes_per_click = 1
 	prints("notes = ", notes)
 	prints("notes per click = ", notes_per_click)
 	$Button2.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	$Label.text = str(notes)
+func _process(_delta):
+	$notes_counter.text = str(notes)
 	
 	if(notes >= 10):
 		$Button2.visible = true
@@ -20,4 +22,5 @@ func _on_button_pressed():
 	notes += notes_per_click
 
 func _on_button_2_visibility_changed():
-	notes_per_click += 1
+	if($Button2.visible == true):
+		notes_per_click += 1
